28b637b5e4209e9430566def3e47d1a991f17565
gz101 <gabrielzhenchiong@gmail.com>
2021-07-29 11:16:43 +1000

    update homepage

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,7 @@ const Body = () => {
   return (
     <>
       <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
-        <img src={ImgMud} alt='hands' className='h-full rounded mb-20 shadow' />
+        <img src={ImgMud} alt='hands' className='h-4/5 rounded mb-20 shadow' />
         <div className='flex flex-col justify-center items-center'>
           <h2 className='text-2xl mb-2'>Placeholder 1</h2>
           <p className='mb-2'>Placeholder text for the first placeholder.</p>
@@ -14,7 +14,7 @@ const Body = () => {
         </div>
       </div>
       <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
-        <img src={ImgRocks} alt='rocks' className='h-full rounded mb-20 shadow' />
+        <img src={ImgRocks} alt='rocks' className='h-4/5 rounded mb-20 shadow' />
         <div className='flex flex-col justify-center items-center'>
           <h2 className='text-2xl mb-2'>Placeholder 2</h2>
           <p className='mb-2'>Placeholder text for the second placeholder.</p>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className='bg-white h-screen flex flex-col justify-center items-center'>
+    <div 
+      className='bg-gradient-to-r from-green-300 to-yellow-900 h-screen flex flex-col justify-center items-center'
+    >
       <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>
         UNDIRTIFY
       </h1>
       <Link 
-        className='py-6 px-10 bg-green-500 rounded-full text-3xl hover:bg-green-300 transition duration-300 ease-in-out flex items-center animate-bounce'
+        className='py-6 px-10 bg-yellow-400 rounded-full text-3xl hover:bg-yellow-200 transition duration-300 ease-in-out flex items-center animate-bounce'
       >
         Start Logging
         <svg 
